3f666d92c99a5ff34c05f02ffe5ca4c052aff7f5
ShoheiTakino <shohei0522penguin@gmail.com>
2023-07-21 23:34:57 +0900

    ✨画面遷移を可能に

--- src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
+++ src/main/java/com/websarva/wings/android/intentsample/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.websarva.wings.android.intentsample
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ListView
 import android.widget.SimpleAdapter
 
@@ -9,6 +12,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupListViewAdapter()
+    }
+
+    private fun setupListViewAdapter() {
         val lvMenu = findViewById<ListView>(R.id.lvMenu)
         val menuList: MutableList<MutableMap<String, String>> = mutableListOf()
         var menu = mutableMapOf("name" to "から揚げ定食", "price" to "800円")
@@ -31,5 +38,22 @@ class MainActivity : AppCompatActivity() {
         val to = intArrayOf(android.R.id.text1, android.R.id.text2)
         val adapter = SimpleAdapter(this, menuList, android.R.layout.simple_list_item_2, from, to)
         lvMenu.adapter = adapter
+        setupClickListener(lvMenu)
+    }
+
+    private fun setupClickListener(view: ListView) {
+        view.onItemClickListener = ListItemClickListener()
+    }
+
+    private inner class ListItemClickListener: AdapterView.OnItemClickListener {
+        override fun onItemClick(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+            val item = parent.getItemAtPosition(position) as MutableMap<String, String>
+            val menuName = item["name"]
+            val menuPrice = item["price"]
+            val intent2MenuThanks = Intent(this@MainActivity, MenuThanksActivity::class.java)
+            intent2MenuThanks.putExtra("menuName", menuName)
+            intent2MenuThanks.putExtra("menuPrice", menuPrice)
+            startActivity(intent2MenuThanks)
+        }
     }
 }
